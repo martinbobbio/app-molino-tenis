f56a03f500000d18d76088cbf056f3bd33ff0069
Martin Bobbio <martinbobbio1@gmail.com>
2020-04-20 21:10:19 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  apiUrl:"http://192.168.99.100:4201",
   backUrl:"http://192.168.99.100:8081",
-  uploaderUrl:"http://192.168.99.100:8081/_uploader/users/upload",
+  pathBackImg:"http://192.168.99.100:8081/_uploader/users/upload"
 };
